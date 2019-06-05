b42bba9d561ace404eea23c134f4efa47ad59828
tu3816329 <tu3816329@gmail.com>
2019-06-05 15:26:40 +0700

    Add files via upload

--- Excercise/Page/Exercise_4_Login.php
+++ Excercise/Page/Exercise_4_Login.php
@@ -8,15 +8,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Exercise 4 Login</title>
-        <style>
-            #information{
-                display: none;
-            }
-            #loginForm{
-                display: block;
-            }
-        </style>
+        <title>Exercise 4 Login</title>      
     </head>
     <body>
         <div id="loginForm">
@@ -30,36 +22,38 @@ and open the template in the editor.
                 <input type="submit" value="Login" name="btAction" />
             </form>
         </div>
-        <div id="information">
-            <h1>User Information</h1>
-            <table border="0">                    
-                <tbody>
-                    <tr>
-                        <td><b>Username</b></td>
-                        <td><?php echo $_SESSION['user']['username'] ?? "" ?></td>
-                    </tr>
 
-                    <tr>
-                        <td><b>Email</b></td>
-                        <td><?php echo $_SESSION['user']['email'] ?? "" ?></td>
-                    </tr>
-                    <tr>
-                        <td><b>Fullname</b></td>
-                        <td><?php echo $_SESSION['user']['fullname'] ?? "" ?></td>
-                    </tr>
-                    <tr>
-                        <td><b>Date Of Birth</b></td>
-                        <td><?php echo $_SESSION['user']['birthday'] ?? "" ?></td>
-                    </tr>                     
-                </tbody>
-            </table>
-
-        </div>
         <?php
         $loginOK = $_SESSION['loginOK'] ?? false;
         if ($loginOK === true) {
-            $hidden = '<style type="text/css">#loginForm{display:none;} #information{display:block}</style>';
+            $hidden = '<style type="text/css">#loginForm{display:none;}</style>';
             echo $hidden;
+            ?><div id="information">
+                <h1>User Information</h1>
+                <table border="0">                    
+                    <tbody>
+                        <tr>
+                            <td><b>Username</b></td>
+                            <td><?php echo $_SESSION['user']['username'] ?? "" ?></td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Email</b></td>
+                            <td><?php echo $_SESSION['user']['email'] ?? "" ?></td>
+                        </tr>
+                        <tr>
+                            <td><b>Fullname</b></td>
+                            <td><?php echo $_SESSION['user']['fullname'] ?? "" ?></td>
+                        </tr>
+                        <tr>
+                            <td><b>Date Of Birth</b></td>
+                            <td><?php echo $_SESSION['user']['birthday'] ?? "" ?></td>
+                        </tr>                     
+                    </tbody>
+                </table>
+
+            </div>
+            <?php
         } else {
             
         }
